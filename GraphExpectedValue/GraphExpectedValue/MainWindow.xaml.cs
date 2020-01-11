f5ee9f7dcb9015e2873bc527ed2c6094715c757b
Георгий Корнилов <gmkornilov@edu.hse.ru>
2020-01-11 14:46:20 +0300

    first tries to create vertex class

--- GraphExpectedValue/GraphExpectedValue/MainWindow.xaml.cs
+++ GraphExpectedValue/GraphExpectedValue/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,66 @@ namespace GraphExpectedValue
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Vertex vertex;
         public MainWindow()
         {
             InitializeComponent();
+            vertex = new Vertex(1);
+            testCanvas.Children.Add(vertex);
+            //Point? dragStart = null;
+
+            //MouseButtonEventHandler mouseDown = (sender, args) => {
+            //    var element = (UIElement)sender;
+            //    dragStart = args.GetPosition(element);
+            //    element.CaptureMouse();
+            //};
+            //MouseButtonEventHandler mouseUp = (sender, args) => {
+            //    var element = (UIElement)sender;
+            //    dragStart = null;
+            //    element.ReleaseMouseCapture();
+            //};
+            //MouseEventHandler mouseMove = (sender, args) => {
+            //    if (dragStart != null && args.LeftButton == MouseButtonState.Pressed)
+            //    {
+            //        var element = (UIElement)sender;
+            //        var p2 = args.GetPosition(testCanvas);
+            //        if (dragStart != null && p2.X > 0 && p2.Y > 0 && p2.X < testCanvas.ActualWidth && p2.Y < testCanvas.ActualHeight)
+            //        {
+            //            Canvas.SetLeft(element, p2.X - dragStart.Value.X);
+            //            Canvas.SetTop(element, p2.Y - dragStart.Value.Y);
+            //        }
+            //    }
+            //};
+            //Action<UIElement> enableDrag = (element) => {
+            //    element.MouseDown += mouseDown;
+            //    element.MouseMove += mouseMove;
+            //    element.MouseUp += mouseUp;
+            //};
+            //var shapes = new UIElement[] {
+            //    new Ellipse() { Fill = Brushes.DarkKhaki, Width = 100, Height = 100 },
+            //    new Rectangle() { Fill = Brushes.LawnGreen, Width = 200, Height = 100 },
+            //};
+
+
+            //foreach (var shape in shapes)
+            //{
+            //    testCanvas.Children.Add(shape);
+            //}
+
+            //foreach (UIElement child in testCanvas.Children)
+            //{
+            //    enableDrag(child);
+            //}
+        }
+
+        private void AddEdgeButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            vertex.Number++;
+        }
+
+        private void RemoveEdgeButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            vertex.Number--;
         }
     }
 }
---
+++ GraphExpectedValue/GraphExpectedValue/Vertex.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GraphExpectedValue
+{
+    public enum VertexType
+    {
+        StartVertex,
+        EndVertex,
+        PathVertex
+    }
+    /// <summary>
+    /// Interaction logic for Vertex.xaml
+    /// </summary>
+    public partial class Vertex : UserControl
+    {
+        private static readonly SolidColorBrush BlackColorBrush = new SolidColorBrush(Colors.Black);
+        private static readonly SolidColorBrush RedColorBrush = new SolidColorBrush(Colors.Red);
+        private static readonly SolidColorBrush BlueColorBrush = new SolidColorBrush(Colors.Blue);
+
+        public static DependencyProperty NumberProperty;
+        public static DependencyProperty VertexTypeProperty;
+        public static DependencyProperty ColorProperty;
+
+        public int Number
+        {
+            get => (int) GetValue(NumberProperty);
+            set => SetValue(NumberProperty, value);
+        }
+
+        public Color CircleColor
+        {
+            get => (Color) GetValue(ColorProperty);
+            set => SetValue(ColorProperty, value);
+        }
+
+        public VertexType VertexType
+        {
+            get => (VertexType) GetValue(VertexTypeProperty);
+            set
+            {
+                switch (value)
+                {
+                    case VertexType.EndVertex:
+                        CircleColor = Colors.Blue;
+                        //Background = BlueColorBrush;
+                        break;
+                    case VertexType.PathVertex:
+                        CircleColor = Colors.Black;
+                        //Background = BlackColorBrush;
+                        break;
+                    case VertexType.StartVertex:
+                        CircleColor = Colors.Red;
+                        //Background = RedColorBrush;
+                        break;
+                }
+                SetValue(VertexTypeProperty, value);
+            }
+        }
+
+        static Vertex()
+        {
+            NumberProperty = DependencyProperty.Register(
+                "Number",
+                typeof(int),
+                typeof(Vertex)
+            );
+            VertexTypeProperty = DependencyProperty.Register(
+                "VertexType",
+                typeof(VertexType),
+                typeof(Vertex)
+            );
+            ColorProperty = DependencyProperty.Register(
+                "Color",
+                typeof(Color),
+                typeof(Vertex),
+                new FrameworkPropertyMetadata(
+                    new PropertyChangedCallback(OnCircleColorChanged)
+                )
+            );
+        }
+
+        private static void OnCircleColorChanged(DependencyObject sender,
+            DependencyPropertyChangedEventArgs e)
+        {
+            Color newColor = (Color) e.NewValue;
+            Vertex vertex = (Vertex) sender;
+            vertex.CircleColor = newColor;
+        }
+        public Vertex()
+        {
+            VertexType = VertexType.StartVertex;
+            //CircleColor = Colors.Orange;
+
+            InitializeComponent();
+        }
+        public Vertex(int number):this()
+        {
+            Number = number;
+        }
+    }
+}
