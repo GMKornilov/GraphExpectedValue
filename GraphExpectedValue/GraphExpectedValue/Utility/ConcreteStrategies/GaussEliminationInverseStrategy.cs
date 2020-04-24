6c361ec05f3bcbb300c4949479c2f1d68475e177
Георгий Корнилов <gmkornilov@edu.hse.ru>
2020-04-24 11:48:03 +0300

    added inverse strategy using gauss elimination

---
+++ GraphExpectedValue/GraphExpectedValue/Utility/ConcreteStrategies/GaussEliminationInverseStrategy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GraphExpectedValue.Utility.ConcreteStrategies
+{
+    public class GaussEliminationInverseStrategy : InverseStrategy
+    {
+        private const double EPS = 1e-6;
+        public Matrix Inverse(Matrix matrix)
+        {
+            var matrixCopy = new Matrix(matrix.Rows, matrix.Cols * 2);
+            for (var i = 0; i < matrix.Rows; i++)
+            {
+                matrixCopy[i, i + matrix.Cols] = 1;
+                for (var j = 0; j < matrix.Cols; j++)
+                {
+                    matrixCopy[i, j] = matrix[i, j];
+                }
+            }
+            matrixCopy.GaussElimination();
+            for (var i = 0; i < matrixCopy.Rows; i++)
+            {
+                if (Math.Abs(matrixCopy[i, i] - 1) > EPS)
+                {
+                    throw new ArgumentException("Given matrix is uninvertible");
+                }
+            }
+            var result = new Matrix(matrix.Rows, matrix.Cols);
+            for (var i = 0; i < matrix.Rows; i++)
+            {
+                for (var j = 0; j < matrix.Cols; j++)
+                {
+                    result[i, j] = matrixCopy[i, j + matrix.Cols];
+                }
+            }
+
+            return result;
+        }
+    }
+}
